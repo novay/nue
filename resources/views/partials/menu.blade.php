6a857935ad91dedda0e3e7f1210709e98f9074cb
Noviyanto Rahmadi <noviantorahmadi@gmail.com>
2022-06-22 04:58:30 +0800

    Update menu.blade.php

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -18,7 +18,7 @@
         </div>
     @else
         <div class="nav-item">
-            <a class="nav-link dropdown-toggle collapsed {{ '/' . request()->segment(1) == Request::route()->getPrefix() ? 'active' : '' }}" href="#navbar-nue-{{ $item['id'] }}" role="button" data-bs-toggle="collapse" data-bs-target="#navbar-nue-{{ $item['id'] }}" aria-expanded="false" aria-controls="navbar-nue-{{ $item['id'] }}">
+            <a class="nav-link dropdown-toggle collapsed {{ request()->segment(1) == $item['uri'] ? 'active' : '' }}" href="#navbar-nue-{{ $item['id'] }}" role="button" data-bs-toggle="collapse" data-bs-target="#navbar-nue-{{ $item['id'] }}" aria-expanded="false" aria-controls="navbar-nue-{{ $item['id'] }}">
                 <i class="iconify nav-icon" data-icon="{{ $item['icon'] }}"></i>
                 <span class="nav-link-title">
                     @if (Lang::has($titleTranslation = 'nue.menu_titles.' . trim(str_replace(' ', '_', strtolower($item['title'])))))
@@ -28,7 +28,7 @@
                     @endif
                 </span>
             </a>
-            <div id="navbar-nue-{{ $item['id'] }}" class="nav-collapse collapse {{ '/' . request()->segment(1) == Request::route()->getPrefix() ? 'show' : '' }}" data-bs-parent="#navbar-menu-page-{{ $item['id'] }}" hs-parent-area="#navbar-menu">
+            <div id="navbar-nue-{{ $item['id'] }}" class="nav-collapse collapse {{ request()->segment(1) == $item['uri'] ? 'show' : '' }}" data-bs-parent="#navbar-menu-page-{{ $item['id'] }}" hs-parent-area="#navbar-menu">
                 @foreach($item['children'] as $item)
                     @include('nue::partials.menu', $item)
                 @endforeach
